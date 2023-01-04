ab046e2f0205f860fba77e72734a337f2c1e3809
Bahaazyoud <bahaalzyoud@digisolfze.com>
2023-01-04 05:05:38 +0300

    array

---
+++ Array/src/Array/Main.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] cars = new String[5];
+		cars[0] = "BMW";
+		cars[1] = "TESLA";
+		cars[2] = "CAMARO";
+		
+		for(int i=0;i<cars.length;i++) {
+			System.out.println(cars[i]);
+		}
+	}
+
+}
