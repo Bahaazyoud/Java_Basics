6745fe32b41ba7b2b70bfd8a93cbf22fa3ea9280
Bahaazyoud <bahaalzyoud@digisolfze.com>
2023-02-12 18:43:05 +0300

    method

---
+++ Method/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(sum(a,b));
+		System.out.println(multiplication(a,b));
+	}
+	static int sum(int num1 ,int num2) {
+		 return num1+num2;
+	}
+	static int multiplication(int num1 , int num2) {
+		return num1 * num2;
+	}
+}
