e4190143ace4b42fd55d0276abf021b5fdcb0201
Bahaazyoud <bahaalzyoud@digisolfze.com>
2023-02-12 18:03:58 +0300

    foreach

---
+++ ForEach/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> food = new ArrayList<String>();
+		
+		food.add("makmoura");
+		food.add("mansaf");
+		food.add("mloukhea");
+		
+		for(String i : food) {
+			System.out.println(i);
+		}
+	}
+
+}
