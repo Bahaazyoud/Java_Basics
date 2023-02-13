ec28d73dc0bf5d132eda38205b3b96fcab0ebc07
Bahaazyoud <bahaalzyoud@digisolfze.com>
2023-02-13 18:02:46 +0300

    printf

---
+++ printf_Method/src/printf_Method/Main.java
@@ -0,0 +1,14 @@
+package printf_Method;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "bahaa";
+		int age = -22;
+		System.out.printf("my name is %s",name);
+		System.out.println();
+		System.out.printf("my age is %+d",age);
+	}
+
+}
